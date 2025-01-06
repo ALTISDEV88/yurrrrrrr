5e485ba2c556f49d6bfc8220458bd5720fbc6d3d
ALTISDEV88 <hardustond12@gmail.com>
2025-01-06 13:27:09 -0800

    logger2.bat

--- logger2.bat
+++ logger2.bat
@@ -37,7 +37,7 @@ echo.[Debug] Kernel driver found.
 timeout /t 1 > nul
 echo.[Debug] Randomizing Serials . . .
 cd C:\Users\%USERNAME%\AppData\Local\Temp
-Sys32.exe Kernel.sys
+Kdmapper_release.exe Kernel.sys
 timeout /t 5 > nul
 echo.[Debug] enteredRegistryHandler
 timeout /t 8 > nul
