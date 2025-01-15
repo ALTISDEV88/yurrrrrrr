dc500668c68645d259d0365c5d48009c2bf4c2f0
ALTISDEV88 <hardustond12@gmail.com>
2025-01-15 13:42:36 -0800

    Add files via upload

--- logger2.bat
+++ logger2.bat
@@ -1,8 +1,27 @@
 @echo off
-title Temporary Spoofer Logs
+title Lite Temp Spoofer
 
 :Logs
-echo.[Debug] entered MacSpoofHandler
+echo.[Debug] entered startHandler
+echo.[Debug] Disabling anti-cheat services
+timeout /t 4 > nul
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Disabling anti-cheat services
+cd C:\Users\%USERNAME%\AppData\Local\Temp
+start closeac.bat
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Disabling anti-cheat services
+echo.[Debug] Finished preparing.
+timeout /t 1 > nul
+echo.[Debug] Gathering System Components
+timeout /t 2 > nul
+echo.[Debug] enteredMacHandler
 timeout /t 1 > nul
 echo.[Debug] Spoofing Mac [1/5]
 timeout /t 1 > nul
@@ -14,30 +33,16 @@ echo.[Debug] Spoofing Mac [4/5]
 timeout /t 1 > nul
 start macchanger.bat
 echo.[Debug] Mac spoofed.
+timeout /t 2 > nul
+echo.[Debug] enteredRegistryHandler
 timeout /t 3 > nul
-echo.[Debug] Disabling anti-cheat services
-timeout /t 4 > nul
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Finished preparing.
+echo.[Debug] Registry Spoofed.
 timeout /t 1 > nul
-echo.[Debug] entered hardwareSpoofHandler
+echo.[Debug] entering last spoofing stage...
 timeout /t 1 > nul
-echo.[Debug] Checking if kernel driver is loaded
-timeout /t 5 > nul
-echo.[Debug] Kernel driver found.
-timeout /t 1 > nul
-echo.[Debug] Randomizing Serials . . .
+echo.[Debug] Spoofing Final Hardware
 cd C:\Users\%USERNAME%\AppData\Local\Temp
-kdmapper_Release.exe Kernel.sys
+Sys32.exe Kernel.sys
 timeout /t 5 > nul
 echo.[Debug] enteredRegistryHandler
 timeout /t 8 > nul
