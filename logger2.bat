a40e3ecc4c571d32ba97b4e2f401a99a427e4803
ALTISDEV88 <hardustond12@gmail.com>
2025-01-16 05:34:48 -0800

    Add files via upload

--- logger2.bat
+++ logger2.bat
@@ -2,48 +2,26 @@
 title Lite Temp Spoofer
 
 :Logs
-echo.[Debug] entered startHandler
-echo.[Debug] Disabling anti-cheat services
-timeout /t 4 > nul
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Disabling anti-cheat services
-cd C:\Users\%USERNAME%\AppData\Local\Temp
+echo.[debug] Killing anti cheats. 
+cd C:\Users\%USERNAME%\Appdata\Local\Temp
 start closeac.bat
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Disabling anti-cheat services
-echo.[Debug] Finished preparing.
+timeout /t 5 > nul
+cls
 timeout /t 1 > nul
-echo.[Debug] Gathering System Components
+echo.[debug] Gathering System Components
 timeout /t 2 > nul
-echo.[Debug] enteredMacHandler
-timeout /t 1 > nul
-echo.[Debug] Spoofing Mac [1/5]
-timeout /t 1 > nul
-echo.[Debug] Spoofing Mac [2/5]
-timeout /t 3 > nul
-echo.[Debug] Spoofing Mac [3/5]
-timeout /t 2 > nul
-echo.[Debug] Spoofing Mac [4/5]
+cls
+echo.[debug] Spoofing Mac
 timeout /t 1 > nul
 start macchanger.bat
-echo.[Debug] Mac spoofed.
-timeout /t 2 > nul
-echo.[Debug] enteredRegistryHandler
-timeout /t 3 > nul
-echo.[Debug] Registry Spoofed.
+timeout /t 3> nul
+cls
 timeout /t 1 > nul
-echo.[Debug] entering last spoofing stage...
-timeout /t 1 > nul
-echo.[Debug] Spoofing Final Hardware
-cd C:\Users\%USERNAME%\AppData\Local\Temp
+echo.[debug] Spoofing final serials
+cd C:\Users\%USERNAME%\Appdata\Local\Temp
 kdmapper_Release.exe Kernel.sys
-timeout /t 5 > nul
+timeout /t 2 > nul
+cls
 
 
 
